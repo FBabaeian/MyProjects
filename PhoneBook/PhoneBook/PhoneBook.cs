1beb7b033746950f2d0de277bec270213052635e
FBabaeian <36853417+FBabaeian@users.noreply.github.com>
2018-05-09 01:13:50 +0430

    PhoneBook

---
+++ PhoneBook/PhoneBook/PhoneBook.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+	class PhoneBook
+	{
+		public  Contact[] contacts;
+
+		public PhoneBook()
+		{
+			contacts = new Contact[2]; ;
+		}
+
+		public bool AddEntry(string name, string family,string phone)
+		{
+			if (!ContainsEntry(name))
+			{
+			
+				Contact AddContact = new Contact(name, family,phone);
+				for (int i = 0; i < contacts.Length; i++)
+				{
+					if (contacts[i] == null)
+					{
+						contacts[i] = AddContact;
+						Console.WriteLine("Phone Book updated. Name: {0} -- Family: {1} -- Phone:{2} has been added!", name, family,phone);
+						return true;
+					}
+				}
+				Console.WriteLine($"Cannot add ({name}) to Phone Book since it is full!");
+				return false;
+			}
+				else
+				{
+					Console.WriteLine($"({name}) already exists in PhoneBook!");
+					UpdateContact(name);
+				}
+				return false;
+		}
+
+		public bool UpdateContact(string Name)
+		{
+			Console.Write("Are you sure you would you like to update the Contact? -- Type 'Y' or 'N': ");
+			string userResponse = Console.ReadLine().ToLower();
+			if (userResponse == "y")
+			{
+				Console.Write($"Would you like to update {Name}'s name or family or Phone? TYPE - 'Name' for name and 'Family' for family and 'Phone' for phone: ");
+				string contactToUpdate = Console.ReadLine().ToLower();
+
+				Console.Write($"Please enter changes to the {contactToUpdate} here: ");
+				string updatedContact = Console.ReadLine().Trim();
+				
+
+				int index = GetEntryIndex(Name);
+				switch (contactToUpdate)
+				{
+					case "name":
+						contacts[index].Name = updatedContact;
+						Console.WriteLine($"Contact {Name} updated to {updatedContact}");
+						return true;
+					case "family":
+						contacts[index].Family = updatedContact;
+						Console.WriteLine($"Contact {Name}'s {contactToUpdate} updated to {updatedContact}");
+						return true;
+					case "phone":
+						contacts[index].Phone = updatedContact;
+						Console.WriteLine($"Contact {Name}'s {contactToUpdate} updated to {updatedContact}");
+						return true;
+				}
+			}
+			return false;
+		}
+
+	
+
+		private int GetEntryIndex(string name)
+		{
+			for (int i = 0; i < contacts.Length; i++)
+			{
+				if (contacts[i] != null && contacts[i].Name.ToLower() == name.ToLower())
+					return i;
+			}
+			return -1;
+		}
+
+		private bool ContainsEntry(string name)
+		{
+			return GetEntryIndex(name) != -1;
+		}
+
+		public void RemoveEntry(string name)
+		{
+			var index = GetEntryIndex(name);
+			if (index != -1)
+			{
+				contacts[index] = null;
+				Console.WriteLine("{0} removed from contacts", name);
+			}
+		}
+
+		public string ViewContactsList()
+		{
+			string contactList = "";
+			foreach (Contact contact in contacts)
+			{
+				if (contact == null)
+				{
+					continue;
+				}
+				contactList += String.Format("Name: {0} -- Family: {1} -- phone: {2}" + Environment.NewLine, contact.Name, contact.Family,contact.Phone);
+			}
+			return (contactList != String.Empty) ? contactList : "Your phone Book is empty.";
+		}
+	}
+}
+
---
+++ PhoneBook/PhoneBook/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace PhoneBook
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			PhoneBook phoneBook = new PhoneBook();
+
+			PromptUser();
+
+			void Menu()
+			{
+				Console.WriteLine("TYPE:");
+				Console.WriteLine("'Add' to add a contact: ");
+				Console.WriteLine("'View' to view the list of contacts: ");
+				Console.WriteLine("'Remove' to select and remove a contact: ");
+				Console.WriteLine("'Update' to select and update a contact: ");
+				Console.WriteLine("'Quit' at anytime to exit: ");
+			}
+
+			void AddPhonesBook(string userInput)
+			{
+				string name = "";
+				string family = "";
+				string phone = "";
+				switch (userInput.ToLower())
+				{
+					case "add":
+						Console.Write("Enter a name: ");
+						name = Console.ReadLine().Trim();
+						switch (name)
+						{
+							case "quit":
+								break;
+							default:
+								Console.Write("Enter a family: ");
+								family = Console.ReadLine().Trim();
+								//phone = Console.ReadLine().Trim();
+								switch (family)
+								{
+									case "quit":
+										break;
+									default:
+										Console.Write("Enter a phone: ");
+										phone = Console.ReadLine().Trim();
+										switch (phone)
+										{
+											case "quit":
+												break;
+											default:
+												phoneBook.AddEntry(name, family, phone);
+												break;
+										}
+										break;
+								}
+								
+								
+								break;
+						}
+						break;
+					case "remove":
+						Console.Write("Enter a name to remove: ");
+						name = Console.ReadLine();
+						switch (name)
+						{
+							case "quit":
+								break;
+							default:
+								phoneBook.RemoveEntry(name);
+								break;
+						}
+						break;
+					case "view":
+						Console.WriteLine(phoneBook.ViewContactsList());
+						break;
+					case "update":
+						Console.WriteLine("Please enter the name of the Contact you like to update");
+						name = Console.ReadLine();
+						phoneBook.UpdateContact(name);
+						break;
+				}
+			}
+
+			void PromptUser()
+			{
+				Menu();
+				string userInput = "";
+				while (userInput != "quit")
+				{
+					Console.WriteLine("What would you like to do?");
+					userInput = Console.ReadLine().Trim();
+					AddPhonesBook(userInput);
+				}
+			}
+		}
+	}
+}
